f063dfb5b4a61717db485aa7c65e7f3ad4a050c7
Pablo <pablo.cheron@proton.me>
2024-11-23 10:37:06 +0100

    basic vector

--- src/structures/vector.rs
+++ src/structures/vector.rs
@@ -1,10 +1,20 @@
 
-pub struct Vector<K> {
-    pub x : K,
+// use std::fmt::Debug;
+
+pub struct Vector<T, const N: usize> {
+    components: [T; N],
+}
+
+impl<T: std::fmt::Debug, const N: usize> Vector<T, N> {
+    pub fn new(components: [T; N]) -> Self {
+        Self { components }
     }
 
-impl<K> Vector<K> {
-    pub fn new(x: K) -> Self {
-        Self { x }
+    pub fn size(&self) -> usize {
+        N
+    }
+
+    pub fn print(&self) {
+        println!("{:?}", self.components);
     }
 }
--- tests/test_vector.rs
+++ tests/test_vector.rs
@@ -2,5 +2,11 @@ use matrix::Vector;
 
 #[test]
 fn test_create_vector() {
-    let _ = Vector::new(5);
+    let _ = Vector::new([42]);
+}
+
+#[test]
+fn test_print_vector() {
+    let v = Vector::new([42]);
+    v.print();
 }
