61c3b7aca8fec7de23190770acea8e12a65e98d7
pcheron-root/42_ft_transcendance <pablo.cheron@proton.me>
2024-11-27 13:24:31 +0100

    Matrix class changed

--- src/structures/matrix.rs
+++ src/structures/matrix.rs
@@ -1,21 +1,37 @@
-use crate::structures::vector::Vector;
 use std::fmt::Debug;
 
-pub struct Matrix<T, const N: usize> {
-    rows: [Vector<T, N>; N],
+pub struct Matrix<K, const M: usize, const N: usize> {
+    data: [[K; N]; M],
 }
 
-impl<T, const N: usize> Matrix<T, N>
+impl<K, const M: usize, const N: usize> Matrix<K, M, N>
 where
-    T: Debug,
+    K: Default + Copy,
 {
-    pub fn new(rows: [Vector<T, N>; N]) -> Self {
-        Self { rows }
+    pub fn new(data: [[K; N]; M]) -> Self {
+        Self { data }
     }
 
+    pub fn shape(&self) -> [usize; 2] {
+        [*(&self.data.len()), *(&self.data[0].len())]
+    }
+
+    pub fn is_square(&self) -> bool {
+        let s = &self.shape();
+        if s[0] == s[1] {
+            return true;
+        }
+        false
+    }
+}
+
+impl<K, const M: usize, const N: usize> Matrix<K, M, N>
+where
+    K: Debug,
+{
     pub fn print(&self) {
-        for row in &self.rows {
-            row.print();
+        for row in &self.data {
+            println!("{:?}", row);
         }
     }
 }
--- tests/test_matrix.rs
+++ tests/test_matrix.rs
@@ -1,18 +1,30 @@
 use matrix::Matrix;
-use matrix::structures::vector::Vector;
-
 
 #[test]
-fn test_create_matrix(){
-    let v1 = Vector::new([2, 3]);
-    let v2 = Vector::new([1, 4]);
-    let _ = Matrix::new([v1, v2]);
+fn test_create_matrix() {
+    let _ = Matrix::new([[1, 2], [3, 4]]);
 }
 
 #[test]
-fn test_print_vector() {
-    let v1 = Vector::new([2, 3]);
-    let v2 = Vector::new([1, 4]);
-    let m = Matrix::new([v1, v2]);
+fn test_print_matrix() {
+    let m = Matrix::new([[1, 2], [3, 4]]);
     m.print();
+}
+
+#[test]
+fn test_shape_matrix() {
+    let m = Matrix::new([[1, 2], [3, 4]]);
+    assert_eq!(m.shape(), [2, 2]);
+}
+
+#[test]
+fn test_is_square1_matrix() {
+    let m = Matrix::new([[1, 2], [3, 4]]);
+    assert_eq!(m.is_square(), true);
+}
+
+#[test]
+fn test_is_square2_matrix() {
+    let m = Matrix::new([[1, 2, 3], [4, 5, 6]]);
+    assert_eq!(m.is_square(), false);
 }
