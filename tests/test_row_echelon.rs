a3431887da53c77a850c1e7d74c8ca2c6bdee9c4
pcheron-root <pablo.cheron@proton.me>
2025-09-18 11:49:50 +0200

    ex10 tests, but one fail

---
+++ tests/test_row_echelon.rs
@@ -0,0 +1,48 @@
+
+use matrix::Matrix;
+
+// -----------------------------------------------------------------
+// Exercice 10 - Row echelon
+// -----------------------------------------------------------------
+
+#[test]
+fn test_row_echelon_2x2() {
+    let mat = Matrix::new([[2.0, 1.0],
+                           [4.0, 3.0]]);
+    let ref_mat = mat.row_echelon();
+    assert_eq!(ref_mat, Matrix::new([[2.0, 1.0],
+                                     [0.0, 1.0]]));
+}
+
+#[test]
+fn test_row_echelon_3x3() {
+    let mat = Matrix::new([[1.0, 2.0, 1.0],
+                           [2.0, 4.0, -1.0],
+                           [-1.0, -2.0, 5.0]]);
+    let ref_mat = mat.row_echelon();
+    // Une forme échelonnée possible
+    assert_eq!(ref_mat, Matrix::new([[1.0, 2.0, 1.0],
+                                     [0.0, 0.0, -3.0],
+                                     [0.0, 0.0, 0.0]]));
+}
+
+#[test]
+fn test_row_echelon_already_echelon() {
+    let mat = Matrix::new([[1.0, 2.0, 3.0],
+                           [0.0, 1.0, 4.0],
+                           [0.0, 0.0, 1.0]]);
+    let ref_mat = mat.row_echelon();
+    assert_eq!(ref_mat, mat); // déjà en forme échelonnée
+}
+
+#[test]
+fn test_row_echelon_with_zero_row() {
+    let mat = Matrix::new([[0.0, 2.0, 1.0],
+                           [0.0, 0.0, 0.0],
+                           [1.0, 1.0, 1.0]]);
+    let ref_mat = mat.row_echelon();
+    // Les lignes nulles passent en bas
+    assert_eq!(ref_mat, Matrix::new([[1.0, 1.0, 1.0],
+                                     [0.0, 2.0, 1.0],
+                                     [0.0, 0.0, 0.0]]));
+}
